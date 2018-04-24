3e3e25d0dca654080b8717341a249f9235a66981
Miguel Sampaio <sampaio.veiga@gmail.com>
2018-04-24 17:31:29 +0100

    altered devise views, controller, i18n 4 consistency

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,43 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_locale
+
+  protected
  
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(
+      :sign_up,
+      keys: [
+        :title,
+        :full_name,
+        :employee_id,
+        :email,
+        :phone_number,
+        :cellphone_number,
+        :department_id,
+        :office_location_id,
+        :admin
+      ]
+    )
+    devise_parameter_sanitizer.permit(
+      :account_update,
+      keys: [
+        :title,
+        :full_name,
+        :employee_id,
+        :phone_number,
+        :email,
+        :cellphone_number,
+        :department_id,
+        :office_location_id,
+        :admin
+      ]
+    )
   end
 end
