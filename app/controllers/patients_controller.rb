434bd5026f747da5f71e24da0b073de9b398d323
Miguel Sampaio <sampaio.veiga@gmail.com>
2018-04-26 16:59:54 +0100

    Corrected mailers

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,5 +1,5 @@
 class PatientsController < ApplicationController
-  before_action :authenticate_user!
+  #before_action :authenticate_user!
   before_action :set_patient, only: [ :show, :edit, :update ]
 
   def show
