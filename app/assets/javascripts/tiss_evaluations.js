3197071ccea1039ecc42d44d4f5ee9edfafd925e
Miguel Sampaio da Veiga <40234@ulsne.min-saude.pt>
2018-06-27 11:57:30 +0100

    Bug fixes

--- app/assets/javascripts/tiss_evaluations.js
+++ app/assets/javascripts/tiss_evaluations.js
@@ -1,24 +1,6 @@
 $(document).on("turbolinks:load", function() {
-  
-  //console.log(gon.patient.tiss_evaluations[0].tiss_evaluation.score);
-  //console.log(gon.patient.tiss_evaluations);
 
-  // circles example
-  //var data = [ 10, 200 ];
-  //d3.select("#d3").append('svg')
-  //  .selectAll('circle').data(data)
-  //  .enter().append('circle')
-  //    .attr("cx", function(d) { return d.score })
-  //    .attr("cy", 10)
-  //    .attr("r", 10)
-
-  // working example for 'p'
-  //var data = gon.patient.tiss_evaluations;
-  //d3.select("#d3")
-  //  .selectAll("p").data(data)
-  //  .enter().append("p")
-  //  .text(function(d) { return d.tiss_evaluation.score; });
-
+  // TISS 28 rules 
   $("input#tiss_evaluation_ba_q3_2").click(function() {
     $("input#tiss_evaluation_ba_q4_0").prop('checked',true);
   });
