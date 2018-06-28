f5119a0f4868fa58a2416d029a2393c73e48c36d
Miguel Sampaio da Veiga <40234@ulsne.min-saude.pt>
2018-06-28 11:27:33 +0100

    added locales messages

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,5 +1,5 @@
 ActionMailer::Base.raise_delivery_errors = false
 ActionMailer::Base.smtp_settings = {  
-	address:              '192.168.1.215_',
+	address:              '0.0.0.0',
 	enable_starttls_auto: false,
 }
