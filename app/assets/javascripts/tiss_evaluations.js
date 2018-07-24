48d087f3637b673139726af41f6e673c85f1168f
Miguel Sampaio da Veiga <sampaio.veiga@gmail.com>
2018-07-24 11:51:46 +0100

    Changed back to IE9 compatibility mode

--- app/assets/javascripts/tiss_evaluations.js
+++ app/assets/javascripts/tiss_evaluations.js
@@ -1,12 +1,14 @@
-//$(document).on("turbolinks:load", function() {
+$(document).on("turbolinks:load", function() {
 // ie9 compatibility
-  $(document).ready(function(){
+//$(document).ready(function(){
   // TISS 28 rules 
   $("input#tiss_evaluation_ba_q3_2").click(function() {
     $("input#tiss_evaluation_ba_q4_0").prop('checked',true);
+    console.log("ba_q3_2");
   });
   $("input#tiss_evaluation_ba_q4_3").click(function() {
     $("input#tiss_evaluation_ba_q3_0").prop('checked',true);
+    console.log("ba_q4_3");
   });
 
   $("input#tiss_evaluation_vs_q1_5").click(function() {
